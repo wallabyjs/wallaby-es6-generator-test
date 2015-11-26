2e93a2933fe47cca71051435ad4afe1d86af9298
Artem Govorov <artem.govorov@gmail.com>
2015-11-26 12:59:36 +1000

    Babel 6 note

--- wallaby.js
+++ wallaby.js
@@ -11,6 +11,12 @@ module.exports = function (wallaby) {
         ],
 
         preprocessors: {
+            // NOTE: If you're using Babel 6, you should use `presets: ['es2015']`.
+            // You will also need to
+            // npm install babel-core (and require it instead of babel)
+            // and
+            // npm install babel-preset-es2015
+            // See http://babeljs.io/docs/plugins/preset-es2015/
             '**/*.js': file => require('babel').transform(file.content, {sourceMap: true, optional: ["runtime"] })
         },
 
